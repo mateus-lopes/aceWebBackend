993ec7fe9812781dd2f50ef87bab1593258ec2ed
Mateus Lopes Albano <mateusalbano22@gmail.com>
2023-09-22 04:05:33 -0300

    resolvendo problema makemigrations

--- config/urls.py
+++ config/urls.py
@@ -22,9 +22,5 @@ urlpatterns = [
     # token
     path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
-<<<<<<< HEAD
     path("api/", include(usuario_router.urls)),
 ]
-=======
-]
->>>>>>> 30dca4fdb5323b36b1889beb883fc6befaa2cd81
