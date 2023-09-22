9c2b89fff86f4966ea23198d1a129fd2d579a928
Mateus Lopes Albano <mateusalbano22@gmail.com>
2023-09-22 13:46:52 -0300

    melhorando visualizacao do admin

--- ace_produtos/admin.py
+++ ace_produtos/admin.py
@@ -2,8 +2,31 @@ from django.contrib import admin
 
 from .models import Categoria, Marca, Produto, Autor
 
+@admin.register(Autor)
+class AutorAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'email')
+    search_fields = ('nome', 'email')
+    list_filter = ('nome',)
+    ordering = ('nome', 'email')
 
-admin.site.register(Categoria)
-admin.site.register(Marca)
-admin.site.register(Produto)
-admin.site.register(Autor)
+@admin.register(Categoria)
+class CategoriaAdmin(admin.ModelAdmin):
+    list_display = ('descricao',)
+    search_fields = ('descricao',)
+    list_filter = ('descricao',)
+    ordering = ('descricao',)
+
+@admin.register(Marca)
+class MarcaAdmin(admin.ModelAdmin):
+    list_display = ('nome',)
+    search_fields = ('nome',)
+    list_filter = ('nome',)
+    ordering = ('nome',)
+
+@admin.register(Produto)
+class ProdutoAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'marca', 'categoria')
+    search_fields = ('nome', 'marca__nome', 'categoria__descricao')
+    list_filter = ('marca', 'categoria')
+    ordering = ('nome', 'marca', 'categoria')
+    list_per_page = 25
