485bae5a867f6df71bc3c615a5f826e663322393
Mateus Lopes Albano <mateusalbano22@gmail.com>
2023-10-09 19:40:03 -0300

    resolvendo erro da conexão cm serializer

--- ace_produtos/views/product_view.py
+++ ace_produtos/views/product_view.py
@@ -1,41 +1,44 @@
-from django.shortcuts import render
-from rest_framework.viewsets import ModelViewSet
-from rest_framework.response import Response
+from rest_framework import viewsets, status
 from rest_framework.decorators import action
-from rest_framework import status
+from rest_framework.response import Response
+from ace_produtos.models.product import Product
+from ace_produtos.serializers.product_serializer import (
+    ProductListSerializer,
+    ProductSerializer,
+    ProductDetailSerializer
+)
 
-from ace_produtos.models.product import Product 
-from ace_produtos.serializers.product_serializer import ProductListSerializer, ProductSerializer, ProductDetailSerializer
-
-class ProductViewSet(ModelViewSet):
+class ProductViewSet(viewsets.ModelViewSet):
     queryset = Product.objects.all()
-    def get_serializer_class(self):
-        if self.action == "list":
-            return ProductListSerializer
-        elif self.action == "retrieve":
-            return ProductDetailSerializer
-        return ProductSerializer
+    serializer_class = ProductSerializer
+
+    # def get_serializer_class(self):
+    #     if self.action == "list":
+    #         return ProductListSerializer
+    #     elif self.action == "retrieve":
+    #         return ProductDetailSerializer
+    #     return ProductSerializer
 
     @action(detail=True, methods=['get'])
-    def get_cores(self, request, pk=None):
+    def get_colors(self, request, pk=None):
         product = self.get_object()
-        cores = product.get_cores()
-        return Response({'cores': cores}, status=status.HTTP_200_OK)
+        colors = product.get_colors()
+        return Response({'colors': colors}, status=status.HTTP_200_OK)
 
     @action(detail=True, methods=['post'])
-    def set_cores(self, request, pk=None):
+    def set_colors(self, request, pk=None):
         product = self.get_object()
-        cores = request.data.get('cores', [])
-        product.set_cores(cores)
+        colors = request.data.get('colors', [])
+        product.set_colors(colors)
         product.save()
         return Response({'message': 'Cores definidas com sucesso'}, status=status.HTTP_200_OK)
-    
+
     @action(detail=True, methods=['get'])
     def get_images(self, request, pk=None):
         product = self.get_object()
         images = product.get_images()
         return Response({'images': images}, status=status.HTTP_200_OK)
-    
+
     @action(detail=True, methods=['post'])
     def set_images(self, request, pk=None):
         product = self.get_object()
